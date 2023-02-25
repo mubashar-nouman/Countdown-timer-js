d7231a01fb425bd71b09a103ff2b84ad007fef31
Mubashar Nouman <mubashirrnouman@gmail.com>
2023-02-25 17:05:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let countDownDate = new Date("March 1, 2023 00:00:00").getTime();
+let x = setInterval(function(){
+    let currentDate = new Date().getTime();
+    let distance = countDownDate - currentDate;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000)
